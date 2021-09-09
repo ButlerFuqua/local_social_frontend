1b975aaa5800b0783ca0ab1389c8e92b88b36a1a
ButlerFuqua <butlerfuqua@gmail.com>
2021-09-09 13:25:14 -1000

    Add comming soon landing page

---
+++ layouts/landing.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app dark>
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Creek Bank Social",
+    };
+  },
+  methods: {
+    reloadWindow() {
+      window.location.reload();
+    },
+  },
+  async created() {
+    this.$nuxt.$on("pageTitleChange", (title) => (this.title = title));
+  },
+  beforeDestroy() {
+    this.$nuxt.$off("pageTitleChange");
+  },
+};
+</script>
+
+<style scoped>
+html,
+body {
+  height: 100%;
+  overflow: hidden;
+  overflow: auto;
+}
+</style>
+
