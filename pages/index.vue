bf6486d07b696116261cf0daab917a4f2c85def4
ButlerFuqua <butlerfuqua@gmail.com>
2021-09-09 13:25:43 -1000

    Pull in landing and font changes from dev

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <v-container>
-    <h1>Creek bank Social</h1>
-  </v-container>
+  <div id="container">
+    <v-card class="pa-4 text-center">
+      <h1>Creek bank Social</h1>
+      <p>A locals-only social media app.</p>
+      <p>Check back often for updates.</p>
+      <h3>Coming soon!</h3>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import generateHeadTags from "../lib/generateMeta";
 export default {
+  layout: "landing",
   head: generateHeadTags(
     "Creek bank Social",
     "A social app for citizens of Creek bank, AL.",
@@ -25,4 +31,17 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+#container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: auto;
+  background: url("~assets/images/landing_background.jpg");
+  background-size: cover;
+  background-position: center;
+}
+</style>
 
