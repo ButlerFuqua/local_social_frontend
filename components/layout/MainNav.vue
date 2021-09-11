6d9c06439c5c3a05f21293211c284143fefe0502
ButlerFuqua <butlerfuqua@gmail.com>
2021-09-11 13:33:18 -1000

    Add custom nav to default layout

---
+++ components/layout/MainNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="mainNavContainer" :class="shown ? 'shown' : ''">
+    <div id="mainNavContent" calss="pa-4">
+      <v-btn id="closeBtn" @click="hideNav" class="my-2" fab small dark
+        >X</v-btn
+      >
+      <h1>Main Nav</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  props: ["shown", "hideNav"],
+};
+</script>
+
+<style lang="scss" scoped>
+#mainNavContainer {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  z-index: 6;
+  background: url("~assets/images/landing_background.jpg");
+  background-size: cover;
+  background-position: center;
+
+  right: 100%;
+  opacity: 0;
+  transition: all 0.3s;
+
+  &.shown {
+    right: 0;
+    opacity: 1;
+  }
+}
+#mainNavContent {
+  height: 100%;
+  width: 100%;
+  background-image: linear-gradient(to top, #c471f5cc 0%, #fa71cdb8 100%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#closeBtn {
+  position: absolute;
+  bottom: 10px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,8 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer id="navDrawer" v-model="drawer" fixed app>
-      <v-list>
-        <v-list-item
-          v-for="(navItem, idx) in navItems"
-          :key="idx"
-          :to="navItem.to"
-          router
-          exact
-          :aria-label="`${navItem.title}`"
-        >
-          <v-list-item-action>
-            {{ navItem.emoji }}
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="navItem.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <MainNav :shown="showNav" :hideNav="() => (showNav = false)" />
     <v-app-bar fixed app flat class="grey lighten-3">
-      <v-btn
-        icon
-        :aria-label="`Toggle Side nav`"
-        @click.stop="drawer = !drawer"
-      >
+      <v-btn icon :aria-label="`Toggle Side nav`" @click.stop="showNav = true">
         <menu-icon />
       </v-btn>
       <v-toolbar-title v-text="title" />
@@ -44,14 +22,16 @@
 <script>
 import MenuIcon from "../components/icons/MenuIcon.vue";
 import ReloadIcon from "../components/icons/ReloadIcon";
+import MainNav from "../components/layout/MainNav.vue";
 export default {
   components: {
     ReloadIcon,
     MenuIcon,
+    MainNav,
   },
   data() {
     return {
-      drawer: false,
+      showNav: false,
       fixed: false,
       navItems: [
         {
@@ -78,7 +58,7 @@ export default {
 </script>
 
 <style scoped>
-#navDrawer {
+#navMenu {
   max-height: 100% !important;
 }
 </style>
